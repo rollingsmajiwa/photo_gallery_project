fb29a506bd70d11aa463f7eaf80bbd558941d578
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-09-19 23:17:29 +0300

    Feat: add indext.html, navbar, urls.py

--- gallery_app/views.py
+++ gallery_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {"name":"Rollings Majiwa"}
+    return (request, "index.html", context)
+
+def about(request):
+    context = {"message":"Welcome to My About"}
+    return(request, "about.html", context)

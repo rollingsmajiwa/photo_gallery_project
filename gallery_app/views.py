9f2fc077cc5abd3e100a81530f85847fa4c71694
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-09-20 09:06:27 +0300

    Feat: add gallery.html, authenticate gallery

--- gallery_app/views.py
+++ gallery_app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     context = {"name":"Rollings Majiwa"}
-    return (request, "index.html", context)
+    return render(request, "index.html", context)
 
-def about(request):
-    context = {"message":"Welcome to My About"}
-    return(request, "about.html", context)
+def contact(request):
+    context = {"message":"Contact Us"}
+    return render(request, "contact.html", context)
+
+@login_required
+def gallery(request):
+    context = {"message": "welcome to gallery"}
+    return render(request, 'gallery.html', context)

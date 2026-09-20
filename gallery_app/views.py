c649035fcf4dfcfcaabea5098216db979520d3d3
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-09-20 09:48:07 +0300

    Feat: add a function to handle contact post request

--- gallery_app/views.py
+++ gallery_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import ContactMessage
 
 # Create your views here.
 def home(request):
@@ -8,6 +9,17 @@ def home(request):
 
 def contact(request):
     context = {"message":"Contact Us"}
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+    # saving to database
+        ContactMessage.objects.create(
+            first_name = first_name,
+            email= email,
+            message=message
+        )
+        return render(request, "contact.html", {'success': True})
     return render(request, "contact.html", context)
 
 @login_required

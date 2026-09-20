8a4a202344ad459d90ebc731d99dc98c4f8c57a5
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-09-20 18:06:42 +0300

    Feat: create a register view function

--- gallery_app/views.py
+++ gallery_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import ContactMessage, Photo
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def home(request):
-    context = {"message":"Discover visual stories, landscape views, and creative moments."}
+    photos = Photo.objects.all()
+    context = {"message":"Discover visual stories, landscape views, and creative moments.", "photos": photos}
     return render(request, "index.html", context)
 
 def contact(request):
@@ -27,3 +30,15 @@ def gallery(request):
     photos = Photo.objects.all()
     context = {"message": "welcome to my Gallery", "photos": photos}
     return render(request, 'gallery.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log in the user after signup
+            return redirect('gallery')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'register.html', {'form': form})

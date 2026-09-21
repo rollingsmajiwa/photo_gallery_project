2b8fea14bffd68df2df3db1dffe91f77a339b07a
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-09-21 18:37:31 +0300

    Fix: fix static_root for Render deployment

--- week2_project/settings.py
+++ week2_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/ "static"]
+STATIC_URL = '/static/'
+STATIC_ROOT = [BASE_DIR/ "staticfiles"]
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
